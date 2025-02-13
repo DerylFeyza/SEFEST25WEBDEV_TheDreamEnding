ca29522a297e3ccf0e3831f1f1db62cd28d2325c
Gamer2Language <bagassatwi@gmail.com>
2025-02-13 13:37:53 +0700

    feat: add rating filter to item details and reviews retrieval

--- src/app/rentals/[id]/page.tsx
+++ src/app/rentals/[id]/page.tsx
@@ -4,13 +4,15 @@ import { getItemsByID } from "@/services/Items";
 
 export default async function page({
   params,
+  searchParams,
 }: {
   params: Promise<{ id: string }>;
+  searchParams?: Promise<{ rating: number }>;
 }) {
   const { id } = await params;
+  const { rating } = await searchParams!;
 
-  const result = await getItemsByID(id);
-  console.log(result);
+  const result = await getItemsByID(id, Number(rating));
   if (!result) {
     return <div>Item not found</div>;
   }
--- src/services/Items.ts
+++ src/services/Items.ts
@@ -3,11 +3,12 @@ import prisma from "@/lib/prisma";
 export const getItems = async () => {
   return await prisma.item.findMany({});
 };
-export const getItemsByID = async (id: string) => {
+export const getItemsByID = async (id: string, rating?: number) => {
   return await prisma.item.findUnique({
     where: { id },
     include: {
       reviews: {
+        where: { rating },
         include: {
           user: true,
         },
