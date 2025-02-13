a4d88dffe528cc28183adbf4359d520cfa9273da
DerylFeyza <muhammadderylfeyza@gmail.com>
2025-02-13 12:36:55 +0700

    feat: item form + item list wip

---
+++ src/app/lenders/dashboard/items/add/page.tsx
@@ -0,0 +1,8 @@
+import ItemForm from "../components/item-form";
+export default function page() {
+	return (
+		<div>
+			<ItemForm />
+		</div>
+	);
+}
---
+++ src/types/entities.ts
@@ -0,0 +1,9 @@
+export interface Item {
+	name: string;
+	category: string;
+	price: number;
+	condition: string;
+	amount: number;
+	location: string;
+	description: string;
+}
