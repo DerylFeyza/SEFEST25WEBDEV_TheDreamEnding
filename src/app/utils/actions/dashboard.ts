db9c3d97f23ebdbf2d01343178607bd35f76494e
DerylFeyza <muhammadderylfeyza@gmail.com>
2025-02-14 22:28:05 +0700

    fix: query

--- src/app/utils/actions/dashboard.ts
+++ src/app/utils/actions/dashboard.ts
@@ -1,33 +1,35 @@
-"use server";
+'use server';
 
-import prisma from "@/lib/prisma";
+import prisma from '@/lib/prisma';
 
 export const getDashboardStats = async (ownerId: string) => {
-    try {
-        const totalRevenueResult = await prisma.$queryRaw<{ total: number }[]>`
+  try {
+    const totalRevenueResult = await prisma.$queryRaw<{ total: number }[]>`
             SELECT SUM(i.rent_price) as total 
             FROM "Rental" r
             JOIN "Item" i ON r.item_id = i.id
-            WHERE r.status = 'COMPLETED' 
+            WHERE r.status = 'COMPLETED' OR r.status = 'CONFIRMED' OR r.status = 'ONGOING'
             AND i.owner_id = ${ownerId}
         `;
-        
-        const totalRevenue = Number(totalRevenueResult[0]?.total) || 0;
 
-        const [totalOngoingRent, totalItems, totalOrders] = await Promise.all([
-            prisma.rental.count({
-                where: { 
-                    status: "ONGOING", 
-                    item: { owner_id: ownerId } 
-                }
-            }),
-            prisma.item.count({ where: { owner_id: ownerId } }),
-            prisma.rental.count({
-                where: { item: { owner_id: ownerId } }
-            })
-        ]);
+    const totalRevenue = Number(totalRevenueResult[0]?.total) || 0;
 
-        const summaryRevenue = await prisma.$queryRaw<{ date: string; revenue: number }[]>`
+    const [totalOngoingRent, totalItems, totalOrders] = await Promise.all([
+      prisma.rental.count({
+        where: {
+          status: 'ONGOING',
+          item: { owner_id: ownerId }
+        }
+      }),
+      prisma.item.count({ where: { owner_id: ownerId } }),
+      prisma.rental.count({
+        where: { item: { owner_id: ownerId } }
+      })
+    ]);
+
+    const summaryRevenue = await prisma.$queryRaw<
+      { date: string; revenue: number }[]
+    >`
             SELECT DATE(r.finished_date) as date, SUM(i.rent_price) as revenue
             FROM "Rental" r
             JOIN "Item" i ON r.item_id = i.id
@@ -37,15 +39,15 @@ export const getDashboardStats = async (ownerId: string) => {
             ORDER BY date ASC
         `;
 
-        return {
-            totalRevenue: totalRevenue.toString(),
-            totalOngoingRent,
-            totalItems,
-            totalOrders,
-            summaryRevenue
-        };
-    } catch (error) {
-        console.error(error);
-        return null;
-    }
+    return {
+      totalRevenue: totalRevenue.toString(),
+      totalOngoingRent,
+      totalItems,
+      totalOrders,
+      summaryRevenue
+    };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
