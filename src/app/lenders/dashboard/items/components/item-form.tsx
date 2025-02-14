0abf2bf6ebb2feb8f9204c0f8ab02a6fbfd84b9f
Gamer2Language <bagassatwi@gmail.com>
2025-02-14 09:52:09 +0700

    feat: update item form validation to allow optional images and improve error handling

--- src/app/lenders/dashboard/items/components/item-form.tsx
+++ src/app/lenders/dashboard/items/components/item-form.tsx
@@ -41,15 +41,18 @@ const ACCEPTED_IMAGE_TYPES = [
 const formSchema = z.object({
   image: z
     .any()
-    .refine((files) => files?.length == 1, "Image is required.")
-    .refine(
-      (files) => files?.[0]?.size <= MAX_FILE_SIZE,
-      `Max file size is 5MB.`
-    )
-    .refine(
-      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
-      ".jpg, .jpeg, .png and .webp files are accepted."
-    ),
+    .optional()
+    .nullable()
+    .refine((files) => {
+      if (!files) return true;
+      if (files?.[0]?.size === 0) return true;
+      return files?.[0]?.size <= MAX_FILE_SIZE;
+    }, `Max file size is 5MB.`)
+    .refine((files) => {
+      if (!files) return true;
+      if (files?.[0]?.size === 0) return true;
+      return ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type);
+    }, ".jpg, .jpeg, .png and .webp files are accepted."),
   name: z.string().min(2, {
     message: "Item name must be at least 2 characters.",
   }),
