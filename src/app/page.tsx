b640a4401c93c2b32bc4d9968fc76a2dd067b607
MFavianZaahir <favianzaahir@gmail.com>
2025-02-17 08:16:51 +0700

    auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { Dashboard } from "./components/dashboard"
 import { getAllItems } from "@/app/utils/actions/item"
 import { getAllRentals } from "./utils/database/rental.query"
 import type { Rental, SustainabilityImpact } from "@prisma/client"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/lib/next-auth"
+import { redirect } from "next/navigation"
 
 type ExtendedRental = Rental & {
   item: { name: string }
@@ -9,6 +12,18 @@ type ExtendedRental = Rental & {
 }
 
 export default async function Home() {
+  const session = await getServerSession(authOptions);
+  const userId = session?.user?.id;
+  if (!userId) {
+    redirect('/auth/login');
+  }  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    }
+  }
   const items = await getAllItems()
   const rentalHistory = (await getAllRentals({
     take: 5,
