1a5ab05381a47bc87d5255ee0caf422509621ee3
DerylFeyza <muhammadderylfeyza@gmail.com>
2025-02-19 09:28:14 +0700

    cleanups

---
+++ prisma/migrations/20250218142530_/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "SustainabilityImpact" ALTER COLUMN "carbon_savings" SET DEFAULT 0,
+ALTER COLUMN "waste_reduction" SET DEFAULT 0;
--- src/app/utils/actions/imageUpload.ts
+++ src/app/utils/actions/imageUpload.ts
@@ -1,5 +1,5 @@
-import type { UploadApiResponse } from "cloudinary";
-import cloudinary from "@/lib/cloudinary";
+import type { UploadApiResponse } from 'cloudinary';
+import cloudinary from '@/lib/cloudinary';
 
 export async function imageUploader(file: File) {
   const buffer: Buffer = Buffer.from(await file.arrayBuffer());
@@ -7,30 +7,31 @@ export async function imageUploader(file: File) {
     const upload: UploadApiResponse | undefined = await new Promise(
       (resolve, reject) => {
         cloudinary.uploader
-          .upload_stream({ upload_preset: "sefest" }, (error, uploadResult) => {
+          .upload_stream({ upload_preset: 'sefest' }, (error, uploadResult) => {
             if (error) reject(error);
             return resolve(uploadResult);
           })
           //@ts-expect-error cloudinary types
           .end(buffer?.data ? buffer.data : buffer);
-      },
+      }
     );
 
     if (!upload)
       return {
         success: false,
-        message: "An error occurred while uploading the image",
+        message: 'An error occurred while uploading the image'
       };
 
     return {
       success: true,
-      message: "Upload successful",
-      url: upload.secure_url,
+      message: 'Upload successful',
+      url: upload.secure_url
     };
   } catch (error) {
+    console.log(error);
     return {
       success: false,
-      message: "An error occurred while uploading the image",
+      message: 'An error occurred while uploading the image'
     };
   }
 }
@@ -38,18 +39,19 @@ export async function imageUploader(file: File) {
 export async function handleImageDelete(filename: string) {
   try {
     //@ts-expect-error filename splitting
-    const publicId = filename.split("/").pop().split(".")[0];
+    const publicId = filename.split('/').pop().split('.')[0];
     const deleteResult = await cloudinary.uploader.destroy(publicId);
 
-    if (deleteResult.result === "ok") {
-      return { success: true, message: "Image deleted successfully." };
+    if (deleteResult.result === 'ok') {
+      return { success: true, message: 'Image deleted successfully.' };
     } else {
-      throw new Error("Failed to delete image.");
+      throw new Error('Failed to delete image.');
     }
   } catch (error) {
+    console.log(error);
     return {
       success: false,
-      message: "An error occurred while deleting the image",
+      message: 'An error occurred while deleting the image'
     };
   }
 }
